9c4a02ccd6aff1a78981b5e4ee9e04bf86a63fad
Papryk <patczo94@gmail.com>
2024-06-01 10:56:28 +0200

    fix: add dialectModule to db config

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
