0d6df7be6ab856c3ee1ba1632aa6b22f9125aed2
Papryk <patczo94@gmail.com>
2024-06-01 10:12:35 +0200

    feat: vercel postgres db

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('books', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
